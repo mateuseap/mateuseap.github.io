baf55f3b431151dc6ab6d745ca5c9b51e6bfa2ac
Mateus Elias <meap@cin.ufpe.br>
2022-05-28 19:19:03 -0300

    eslint and prettier configs added, devDependecies updated

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,9 +1,9 @@
 function Home() {
-    return (
-      <div>
-        <h1> mateuseap </h1>
-      </div>
-    );
-  }
-  
-  export default Home;
+  return (
+    <div>
+      <h1> mateuseap </h1>
+    </div>
+  );
+}
+
+export default Home;
