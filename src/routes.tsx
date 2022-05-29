04494254adf4c10b4ef0eb6836d9fb7533e7804b
Mateus Elias <meap@cin.ufpe.br>
2022-05-29 13:17:47 -0300

    About and Projects routes created

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,12 +1,16 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { Links } from './types/enums';
 import Home from './pages/Home/index';
+import About from './pages/About/index';
+import Projects from './pages/Projects/index';
 
 function Rotas() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path={Links.HOME} element={<Home />} />
+        <Route path={Links.ABOUT} element={<About />} />
+        <Route path={Links.PROJECTS} element={<Projects />} />
       </Routes>
     </BrowserRouter>
   );
