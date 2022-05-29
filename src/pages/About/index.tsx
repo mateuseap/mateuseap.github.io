84ec16027f386d1c00a92b5fe9baf399e1ece336
Mateus Elias <meap@cin.ufpe.br>
2022-05-29 13:16:56 -0300

    base of About page created

---
+++ src/pages/About/index.tsx
@@ -0,0 +1,28 @@
+import {
+  Background,
+  TopNavigation,
+  Rectangle1,
+  HeaderTitle,
+  HomeButton,
+  AboutButton,
+  ProjectsButton,
+} from './styles';
+import { Links } from '../../types/enums';
+
+function About() {
+  return (
+    <Background>
+      <TopNavigation>
+        <Rectangle1>😀</Rectangle1>
+        <HeaderTitle> mateuseap </HeaderTitle>
+        <span style={{ whiteSpace: 'nowrap' }}>
+          <HomeButton to={Links.HOME}>Home</HomeButton>
+          <AboutButton to={Links.ABOUT}>Sobre mim</AboutButton>
+          <ProjectsButton to={Links.PROJECTS}>Projetos</ProjectsButton>
+        </span>
+      </TopNavigation>
+    </Background>
+  );
+}
+
+export default About;
