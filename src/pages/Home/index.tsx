062307ab7009f11582380819716908976ac4392e
Mateus Elias <meap@cin.ufpe.br>
2022-05-28 21:31:56 -0300

    basic Home page structure created

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,8 +1,38 @@
+import {
+  Background,
+  TopNavigation,
+  Rectangle1,
+  HeaderTitle,
+  MainText,
+  SecondaryText,
+} from './styles';
+import fox from '../../assets/fox.png';
+
 function Home() {
   return (
-    <div>
-      <h1> mateuseap </h1>
-    </div>
+    <Background>
+      <TopNavigation>
+        <Rectangle1>😀</Rectangle1>
+        <HeaderTitle> mateuseap </HeaderTitle>
+      </TopNavigation>
+      <MainText>
+        Meu nome é Mateus Elias, seja bem vindo ao meu website pessoal.
+      </MainText>
+      <SecondaryText>
+        Ainda estamos em fase construção, em breve o website estará recheado 😎
+      </SecondaryText>
+      <img
+        src={fox}
+        alt='fox'
+        style={{
+          position: 'absolute',
+          width: '45vw',
+          height: '90vh',
+          left: '50vw',
+          top: '10vh',
+        }}
+      />
+    </Background>
   );
 }
 
