be6519df5823e15c13de3bdc9cea3986d95e3259
Mateus Elias <meap@cin.ufpe.br>
2023-04-01 16:11:07 -0300

    fix(src/pages/Home/styles.ts): comments removed

--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -72,63 +72,6 @@ export const HeaderTitle = styled.div`
   color: #ffffff;
 `;
 
-/*export const HomeButton = styled(NavLink)`
-  position: absolute;
-  left: 50vw;
-  top: 14.29%;
-  bottom: 11.9%;
-
-  font-family: 'IBM Plex Mono';
-  font-style: normal;
-  font-weight: 500;
-  font-size: 24px;
-  line-height: 31px;
-  display: flex;
-  align-items: center;
-
-  text-decoration: none;
-
-  color: #ffffff;
-`;
-
-export const AboutButton = styled(NavLink)`
-  position: absolute;
-  left: 55vw;
-  top: 14.29%;
-  bottom: 11.9%;
-
-  font-family: 'IBM Plex Mono';
-  font-style: normal;
-  font-weight: 500;
-  font-size: 24px;
-  line-height: 31px;
-  display: flex;
-  align-items: center;
-
-  text-decoration: none;
-
-  color: #ffffff;
-`;
-
-export const ProjectsButton = styled(NavLink)`
-  position: absolute;
-  left: calc(60.85vw + 31.02px);
-  top: 14.29%;
-  bottom: 11.9%;
-
-  font-family: 'IBM Plex Mono';
-  font-style: normal;
-  font-weight: 500;
-  font-size: 24px;
-  line-height: 31px;
-  display: flex;
-  align-items: center;
-
-  text-decoration: none;
-
-  color: #ffffff;
-`;*/
-
 export const BurgerMenu = styled.div`
   position: absolute;
   right: 0%;
