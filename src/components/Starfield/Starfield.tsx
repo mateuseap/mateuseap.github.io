582fc277468a8202a891c36abbaf059b34ad4490
Mateus Elias <meap@cin.ufpe.br>
2023-09-08 05:58:01 -0300

    feat(src/components): create Starfield component

---
+++ src/components/Starfield/Starfield.tsx
@@ -0,0 +1,43 @@
+import { clsx } from 'clsx';
+import { useReducedMotion } from 'framer-motion';
+import { useEffect, useState } from 'react';
+import { useWindowSize } from 'react-use';
+import { StarField } from 'starfield-react';
+
+export interface StarfieldProps {
+  speed?: number;
+  className?: string;
+}
+
+function Starfield({ speed = 0.5, className = '' }: StarfieldProps) {
+  const { width, height } = useWindowSize();
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  const shouldReduceMotion = useReducedMotion();
+
+  return (
+    <div
+      aria-hidden='true'
+      className={clsx(
+        'pointer-events-none fixed left-0 top-0 -z-10 h-full w-full select-none overflow-hidden',
+        className,
+      )}
+    >
+      {mounted && (
+        <StarField
+          fps={60}
+          width={width}
+          height={height}
+          speed={shouldReduceMotion ? 0 : speed}
+          noBackground
+        />
+      )}
+    </div>
+  );
+}
+
+export default Starfield;
