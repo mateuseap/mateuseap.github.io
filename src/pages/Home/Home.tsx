15bf81d2f19b35046da12b5ae88ca9e31e9ad352
Mateus Elias <meap@cin.ufpe.br>
2023-09-08 07:17:27 -0300

    feat(pages/Home): create 'My Skills' section

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -7,6 +7,31 @@ import {
   AcademicCapIcon,
   MapPinIcon,
 } from '@heroicons/react/24/outline';
+import TechnologyCard, {
+  TechnologyCardProps,
+} from '../../components/TechnologyCard/TechnologyCard';
+import {
+  SiHaskell as HaskellIcon,
+  SiNestjs as NestJSIcon,
+  SiLinux as LinuxIcon,
+  SiJest as JestIcon,
+  SiPostgresql as PostgreSQLIcon,
+  SiFlask as FlaskIcon,
+  SiAmazonaws as AWSIcon,
+} from 'react-icons/si';
+import {
+  TbBrandCpp as CppIcon,
+  TbBrandTypescript as TypeScriptIcon,
+  TbBrandJavascript as JavascriptIcon,
+  TbBrandPython as PythonIcon,
+  TbBrandHtml5 as Html5Icon,
+  TbBrandCss3 as Css3Icon,
+  TbBrandReact as ReactIcon,
+  TbBrandGit as GitIcon,
+  TbBrandDocker as DockerIcon,
+  TbBrandDjango as DjangoIcon,
+} from 'react-icons/tb';
+import { BiLogoJava as JavaIcon, BiLogoGoogleCloud as GoogleCloudIcon } from 'react-icons/bi';
 
 function Home() {
   const medias = [
@@ -29,9 +54,99 @@ function Home() {
 
   const profilePicture = 'https://avatars.githubusercontent.com/u/52019009';
 
+  const usedProgrammingLanguages: Array<TechnologyCardProps> = [
+    {
+      name: 'C',
+    },
+    {
+      name: 'C++',
+    },
+    {
+      name: 'JavaScript',
+      Icon: JavascriptIcon,
+    },
+    {
+      name: 'TypeScript',
+      Icon: TypeScriptIcon,
+    },
+    {
+      name: 'Python',
+      Icon: PythonIcon,
+    },
+    {
+      name: 'Java',
+      Icon: JavaIcon,
+    },
+    {
+      name: 'Haskell',
+      Icon: HaskellIcon,
+    },
+  ];
+
+  const usedFrameworksAndTechnologies: Array<TechnologyCardProps> = [
+    {
+      name: 'HTML5',
+      Icon: Html5Icon,
+    },
+    {
+      name: 'CSS3',
+      Icon: Css3Icon,
+    },
+    {
+      name: 'React',
+      Icon: ReactIcon,
+    },
+    {
+      name: 'NestJS',
+      Icon: NestJSIcon,
+    },
+    {
+      name: 'TypeORM',
+    },
+    {
+      name: 'Linux',
+      Icon: LinuxIcon,
+    },
+    {
+      name: 'Git',
+      Icon: GitIcon,
+    },
+    {
+      name: 'Docker',
+      Icon: DockerIcon,
+    },
+    {
+      name: 'Keycloak'
+    },
+    {
+      name: 'Jest',
+      Icon: JestIcon,
+    },
+    {
+      name: 'PostgreSQL',
+      Icon: PostgreSQLIcon,
+    },
+    {
+      name: 'Flask',
+      Icon: FlaskIcon,
+    },
+    {
+      name: 'Django',
+      Icon: DjangoIcon,
+    },
+    {
+      name: 'AWS',
+      Icon: AWSIcon,
+    },
+    {
+      name: 'GCP',
+      Icon: GoogleCloudIcon,
+    },
+  ];
+
   return (
     <DefaultPage sidebar={false}>
-      <main className='flex flex-col mx-auto min-h-screen max-w-[720px] px-4 pt-40 gap-y-6'>
+      <main className='flex flex-col mx-auto min-h-screen max-w-[720px] px-4 pt-20 gap-y-6'>
         <div className='h-full w-full flex justify-center sm:justify-start items-center text-3xl gap-x-4 font-medium'>
           <img
             src={profilePicture}
@@ -47,7 +162,7 @@ function Home() {
             <div className='text-sm pt-1'>@mateuseap</div>
           </div>
         </div>
-        <div className='h-full w-full font-light'>
+        <div className='h-full w-full font-light mt-2'>
           Passionate about coding and committed to using technology to assist
           and empower others, I actively seek out opportunities to collaborate
           and innovate.
@@ -66,6 +181,27 @@ function Home() {
             <div>Pernambuco - Brazil</div>
           </div>
         </div>
+        <section className='mt-2 mb-20'>
+          <p className='text-xl font-medium'>My Skills</p>
+          <div className='mt-4'>
+            <p className='text-sm font-normal'>Programming Languages</p>
+            <div className='mt-3 grid grid-cols-3 gap-2 sm:grid-cols-5'>
+              {usedProgrammingLanguages.map((tech: TechnologyCardProps) => (
+                <TechnologyCard key={tech.name} {...tech} />
+              ))}
+            </div>
+          </div>
+          <div className='mt-6'>
+            <p className='text-sm font-normal'>Frameworks / Technologies</p>
+            <div className='mt-3 grid grid-cols-3 gap-2 sm:grid-cols-5'>
+              {usedFrameworksAndTechnologies.map(
+                (tech: TechnologyCardProps) => (
+                  <TechnologyCard key={tech.name} {...tech} />
+                ),
+              )}
+            </div>
+          </div>
+        </section>
       </main>
     </DefaultPage>
   );
